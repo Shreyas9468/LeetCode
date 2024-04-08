cfea6b649dbf8e6d6b0dbc1fa162f2a837503d8d
Shreyas <122095667+Shreyas9468@users.noreply.github.com>
2024-04-08 20:41:39 +0530

    Runtime: 105 ms (37.49%), Memory: 12.9 MB (63.58%) - Gitcode

--- 300-longest-increasing-subsequence/300. Longest Increasing Subsequence.cpp
+++ 300-longest-increasing-subsequence/300. Longest Increasing Subsequence.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
-    int lengthOfLIS(std::vector<int>& nums) {
-        // Your logic for finding the length of the longest increasing subsequence goes here
-        // Return the result    
-        int maxlength=INT_MIN;
-        int n=nums.size();
-        vector<int>dp(n,1);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[i]>nums[j]){
-                    dp[i]=max(dp[i],1+dp[j]);
-                }
-            }
-            maxlength=max(maxlength,dp[i]);
+    int lis(int i , vector<int> &a ,vector<int>&dp){
+    if(dp[i]!=-1){
+        return dp[i];
+    }
+    int ans  = 1;
+    for(int j =0;j<i;j++){
+        if(a[i] > a[j]){
+            ans = max(ans , 1 + lis(j , a , dp));
         }
-        return maxlength;
+    }
+    return dp[i] = ans;
+}
+    int lengthOfLIS(vector<int>& a) {
+        int n = a.size();
+        vector<int> dp(n , -1);
+        int ans = 0;
+    for(int i=0;i<n;i++){
+        ans = max(ans, lis(i , a ,dp));
+    }
+    return ans;
     }
 };
+
