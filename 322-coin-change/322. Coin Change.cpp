65be791042edc59c5dcf1efe475465805e9fa719
Shreyas <122095667+Shreyas9468@users.noreply.github.com>
2024-04-14 14:37:56 +0530

    Runtime: 28 ms (61.20%), Memory: 15.9 MB (87.36%) - Gitcode

---
+++ 322-coin-change/322. Coin Change.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int coinChange(std :: vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<int> dp(amount + 1, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < n; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = min(dp[i], 1 + dp[i - coins[j]]);
+                }
+            }
+        }
+        if (dp[amount] == amount + 1) {
+            return -1;
+        }
+        return dp[amount];
+    }
+};
