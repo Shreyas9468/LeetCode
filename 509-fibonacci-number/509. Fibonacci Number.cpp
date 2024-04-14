75d5b3a94835aa5ddd3b443701058ec72547aa29
Shreyas <122095667+Shreyas9468@users.noreply.github.com>
2024-04-14 21:09:35 +0530

    Runtime: 2 ms (79.44%), Memory: 7.2 MB (12.20%) - Gitcode

--- 509-fibonacci-number/509. Fibonacci Number.cpp
+++ 509-fibonacci-number/509. Fibonacci Number.cpp
@@ -13,7 +13,14 @@ public:
 
     int fib(int n) {
         vector<int>dp(n+1,-1);
-        int ans = solve(n,dp);
-        return ans;
+        if(n<2){
+            return n;
+        }
+        dp[0] =0;
+        dp[1] =1;
+        for(int i = 2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
