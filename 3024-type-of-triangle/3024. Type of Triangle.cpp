d4dbd2edf361e92fe506aa6d18d86e84ea976d3a
Shreyas <122095667+Shreyas9468@users.noreply.github.com>
2024-05-03 00:06:20 +0530

    Runtime: 4 ms (59.68%), Memory: 22.2 MB (34.55%) - Gitcode

--- 3024-type-of-triangle/3024. Type of Triangle.cpp
+++ 3024-type-of-triangle/3024. Type of Triangle.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     string triangleType(vector<int>& nums) {
-        // we want the largest side at the end for a + b > c, c should be largest so sort
-        sort(nums.begin(), nums.end());
-        
-        // if a + b <= c then triangle cannot be formed 
-        if(nums[0] + nums[1] <= nums[2]) return "none";
-        
-        // if a == b & a = c then equilateral
-        if(nums[0] == nums[1] && nums[0] == nums[2]) return "equilateral";
-        
-        // if a != b & b != c & c != a then scalene
-        if(nums[0] != nums[1] && nums[0] != nums[2] && nums[2] != nums[1]) return "scalene";
-        
-        // else isosceles
-        return "isosceles";
+       sort(nums.begin(),nums.end());
+       int a = nums[0];
+       int b = nums[1];
+       int c = nums[2];
+       if(a+b <= c){
+        return "none";
+       }
+
+       if(a==b && a==c){
+        return "equilateral";
+       }
+       if(a!=b && a!=c &&b!=c){
+        return "scalene";
+       }
+
+       return "isosceles";
     }
 };
