9a139636b7d539dcb1acac5cce54a2af41deeb74
Shreyas <122095667+Shreyas9468@users.noreply.github.com>
2024-05-03 09:23:16 +0530

    Runtime: 0 ms (100.00%), Memory: 7.7 MB (27.22%) - Gitcode

---
+++ 165-compare-version-numbers/165. Compare Version Numbers.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        string num1="";
+        string num2 = "";
+        vector<long long int>v1;
+        vector<long long int>v2;
+
+        for(auto it :version1 ){
+            if(it!='.'){
+                num1.push_back(it);
+            }
+            else{
+                v1.push_back(stol(num1));
+                num1="";
+            }
+        }
+        v1.push_back(stol(num1));
+        num1="";
+        for(auto it :version2 ){
+            if(it!='.'){
+                num2.push_back(it);
+            }
+            else{
+                v2.push_back(stol(num2));
+                num2="";
+            }
+        }
+        v2.push_back(stol(num2));
+        num2="";
+        while(v1.size()!=v2.size()){
+            if(v1.size()<v2.size()){
+                v1.push_back(0);
+            }
+            else{
+                v2.push_back(0);
+            }
+        }
+        for(int i =0;i<v1.size();i++){
+            if(v1[i]<v2[i]){
+                return -1;
+            }
+            else if(v1[i]>v2[i]){
+                return 1;
+            }
+        }
+
+        return 0;
+
+    }
+};
