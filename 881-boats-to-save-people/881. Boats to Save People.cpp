7e57d2c36a4b545684c640933ed76beda97370c9
Shreyas <122095667+Shreyas9468@users.noreply.github.com>
2024-05-04 08:37:17 +0530

    Runtime: 48 ms (92.37%), Memory: 49.5 MB (8.97%) - Gitcode

--- 881-boats-to-save-people/881. Boats to Save People.cpp
+++ 881-boats-to-save-people/881. Boats to Save People.cpp
@@ -1,3 +1,9 @@
+const int ZERO = []()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	return 0;
+}();
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
