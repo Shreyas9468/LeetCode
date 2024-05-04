b7bd8f12a81c4e80f717a85a571ca2bd1c8a771b
Shreyas <122095667+Shreyas9468@users.noreply.github.com>
2024-05-04 08:36:42 +0530

    Runtime: 65 ms (16.80%), Memory: 49.5 MB (8.97%) - Gitcode

---
+++ 881-boats-to-save-people/881. Boats to Save People.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int ans=0;
+        int n= people.size();
+        sort(people.begin(),people.end());
+        vector<int>st;
+        for(int i=0;i<n;i++){
+            if(people[i]>=limit){
+                ans++;
+            }
+            else{
+                st.push_back(people[i]);
+            }
+
+        }
+        
+        int i=0,j=st.size()-1;
+        while(i<=j){
+           if(st[i]+st[j] <= limit){
+            i++;
+            j--;
+           }
+           else{
+            j--;
+           }
+           ans++;
+           
+        }
+        return ans;
+
+
+        // for(int i=0;i<people.size();i+=2){
+        //     if(i==people.size()-1){
+        //         if(people[i]>limit){
+        //             ans+=2;
+        //         }
+        //         else{
+        //             ans++;
+        //         }
+        //     }
+        //     else{
+        //         int currsum = people[i]+people[i+1];
+        //         if(currsum>limit){
+        //             ans+=2;
+        //         }
+        //         else{
+        //             ans++;
+        //         }
+        //     }
+        // }
+        // return ans;
+    }
+};
