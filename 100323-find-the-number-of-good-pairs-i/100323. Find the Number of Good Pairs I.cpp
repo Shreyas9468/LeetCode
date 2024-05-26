c7d7e6b8fa58b42e35dbf6bc4d2a19420c632468
Shreyas <122095667+Shreyas9468@users.noreply.github.com>
2024-05-26 08:17:43 +0530

    Runtime: 4 ms (100.00%), Memory: 41.3 MB (50.00%) - Gitcode

---
+++ 100323-find-the-number-of-good-pairs-i/100323. Find the Number of Good Pairs I.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numberOfPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        int cnt  = 0;
+        for(int  i= 0;i<nums1.size();i++){
+            for(int j = 0;j<nums2.size();j++){
+                if(nums1[i]%(nums2[j]*k) == 0 ){
+                    cnt++;
+                }
+                if(nums1[i]<nums2[j]){
+                    break;
+                }
+            }
+        }
+        return cnt;
+    }
+};
