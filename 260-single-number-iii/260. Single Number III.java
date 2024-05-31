09ac68e01341394148ed47dcd209d0179a004ece
Shreyas <122095667+Shreyas9468@users.noreply.github.com>
2024-05-31 23:21:11 +0530

    Runtime: 7 ms (18.56%), Memory: 45.8 MB (22.05%) - Gitcode

---
+++ 260-single-number-iii/260. Single Number III.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i=0;i<nums.length;i++){
+            int  n = nums[i];
+             map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+        List<Integer> list = new ArrayList<Integer>();
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            if(entry.getValue()==1){
+                list.add(entry.getKey());
+            }
+        }
+        int[] result = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+        return result;
+    }
+}
