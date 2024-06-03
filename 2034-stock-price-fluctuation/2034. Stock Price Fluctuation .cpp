f0f4200e3978ce1f140d377c1a36004a2b50d604
Shreyas <122095667+Shreyas9468@users.noreply.github.com>
2024-06-03 10:09:03 +0530

    Runtime: 1724 ms (5.18%), Memory: 162.7 MB (98.04%) - Gitcode

--- 2034-stock-price-fluctuation/2034. Stock Price Fluctuation .cpp
+++ 2034-stock-price-fluctuation/2034. Stock Price Fluctuation .cpp
@@ -1,51 +1,72 @@
-class StockPrice
-{
-public:
-    unordered_map<int, int> map;
-    priority_queue<pair<int, int>> PQ;
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> PQ_MIN;
+#include <unordered_map>
+#include <algorithm>
+#include <climits>
 
-    int currentPrice = 0;
-    int currentTime = 0;
+#define ll long long int
+
+class StockPrice {
+public:
+    std::unordered_map<ll, ll> mp;
+    ll curr = 0;
+    ll maxp = LLONG_MIN;
+    ll minp = LLONG_MAX;
+    ll latestTimestamp = 0;
 
     StockPrice() {}
-    void update(int timestamp, int price)
-    {
 
-        map[timestamp] = price;
+    void update(int timestamp, int price) {
+        if (mp.find(timestamp) != mp.end()) {
+            // When updating an existing timestamp
+            int prev = mp[timestamp];
+            mp[timestamp] = price;
 
-        PQ.push({price, timestamp});
-        PQ_MIN.push({price, timestamp});
+            if (timestamp == latestTimestamp) {
+                curr = price;
+            }
 
-        // updating the current
-        if (timestamp >= currentTime)
-        {
-            currentTime = timestamp;
-            currentPrice = price;
-        }
-        // updating the max
-        while (map[PQ.top().second] != PQ.top().first)
-        {
-            PQ.pop();
-        }
-        // updating the min
-        while (map[PQ_MIN.top().second] != PQ_MIN.top().first)
-        {
-            PQ_MIN.pop();
+            if (prev == maxp || prev == minp) {
+                // Recalculate maxp and minp if the current max or min is being changed
+                maxp = LLONG_MIN;
+                minp = LLONG_MAX;
+                for (auto& it : mp) {
+                    maxp = std::max(it.second, maxp);
+                    minp = std::min(it.second, minp);
+                }
+            } else {
+                // Otherwise, update maxp and minp directly
+                maxp = std::max(maxp, 1LL * price);
+                minp = std::min(minp, 1LL * price);
+            }
+        } else {
+            // When adding a new timestamp
+            mp[timestamp] = price;
+            if (timestamp > latestTimestamp) {
+                latestTimestamp = timestamp;
+                curr = price;
+            }
+            maxp = std::max(maxp, 1LL * price);
+            minp = std::min(minp, 1LL * price);
         }
     }
-    int current()
-    {
-        return currentPrice;
+
+    int current() {
+        return curr;
     }
 
-    int maximum()
-    {
-        return PQ.top().first;
+    int maximum() {
+        return maxp;
     }
 
-    int minimum()
-    {
-        return PQ_MIN.top().first;
+    int minimum() {
+        return minp;
     }
 };
+
+/**
+ * Your StockPrice object will be instantiated and called as such:
+ * StockPrice* obj = new StockPrice();
+ * obj->update(timestamp,price);
+ * int param_2 = obj->current();
+ * int param_3 = obj->maximum();
+ * int param_4 = obj->minimum();
+ */
