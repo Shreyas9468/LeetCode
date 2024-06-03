d772961ade1c11b4c7c0c2e718ff407e048f0165
Shreyas <122095667+Shreyas9468@users.noreply.github.com>
2024-06-03 13:08:45 +0530

    Runtime: 0 ms (100.00%), Memory: 40.9 MB (83.76%) - Gitcode

---
+++ 225-implement-stack-using-queues/225. Implement Stack using Queues.java
@@ -0,0 +1,35 @@
+class MyStack {
+      private Queue<Integer> que;
+    public MyStack() {
+         que = new LinkedList<>();
+    }
+    
+   public void push(int x) {
+    que.add(x);
+        for (int i = 0; i < que.size()-1; i++){
+            que.add(que.poll());
+        }
+    }
+
+
+    public int pop() {
+       return  que.poll();
+    }
+
+    public int top() {
+        return que.peek();
+    }
+
+    public boolean empty() {
+        return que.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
