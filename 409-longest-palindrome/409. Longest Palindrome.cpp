b61bcdda3b34435d4ff62a526242bef2dd773373
Shreyas <122095667+Shreyas9468@users.noreply.github.com>
2024-06-04 09:14:44 +0530

    Runtime: 3 ms (55.35%), Memory: 6.9 MB (100.00%) - Gitcode

---
+++ 409-longest-palindrome/409. Longest Palindrome.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+   int longestPalindrome(string s) {
+    int odds = 0;
+    for (char c='A'; c<='z'; c++)
+        odds += count(s.begin(), s.end(), c) & 1;
+    return s.size() - odds + (odds > 0);// returns bool (odds > 0) which is going to conc=vert into 1
+}
+};
