c8f1566e4f39762ab6adad565266011fa465dd63
Shreyas <122095667+Shreyas9468@users.noreply.github.com>
2024-06-22 10:57:16 +0530

    Runtime: 189 ms (70.20%), Memory: 237.9 MB (81.66%) - Gitcode

---
+++ 2750-ways-to-split-array-into-good-subarrays/2750. Ways to Split Array Into Good Subarrays.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numberOfGoodSubarraySplits(vector<int>& nums) {
+        long ans = 0;
+        int oc = 0;
+        int zc = 0;
+        int n = nums.size();
+        int mod = 1e9+7;
+        for(int i =0;i<n;i++){
+            if(nums[i] == 0){
+                zc++;
+            }
+            else{
+                oc++;
+                if(oc == 1){
+                    ans = 1;
+                }
+                else{
+                    ans = (ans*(zc+1)%mod)%mod;
+                }
+                zc = 0;
+            }
+        }
+        return (int) ans;
+    }
+};
