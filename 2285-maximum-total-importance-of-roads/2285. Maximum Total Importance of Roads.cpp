ccc87b32d3565a3a203f9f2fda7604b04b36944c
Shreyas <122095667+Shreyas9468@users.noreply.github.com>
2024-06-28 18:57:26 +0530

    Runtime: 424 ms (23.11%), Memory: 156.3 MB (26.73%) - Gitcode

---
+++ 2285-maximum-total-importance-of-roads/2285. Maximum Total Importance of Roads.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    long long maximumImportance(int n, vector<vector<int>>& r) {
+        //
+        vector<int>v(n,0);
+        for(auto it :r ){
+            v[it[0]]++;
+            v[it[1]]++;
+        }
+        priority_queue<pair<int,int>>pq;
+        for(int i =0;i<n;i++){
+            pq.push({v[i] , i});
+        }
+        int cuimp = n;
+        vector<int>st(n,0);
+        while(!pq.empty()){
+            int ind = pq.top().second;
+            pq.pop();
+            st[ind] = cuimp--;
+        }
+        long long sum =0;
+        for(auto it : r){
+            sum+= (st[it[0]] + st[it[1]]);
+        }
+        return sum;
+
+    }
+};
