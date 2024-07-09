af513e91c1615da7c138366c5b74b14713a0b97b
Shreyas <122095667+Shreyas9468@users.noreply.github.com>
2024-07-09 08:26:22 +0530

    Runtime: 159 ms (35.88%), Memory: 86.9 MB (6.47%) - Gitcode

--- 1701-average-waiting-time/1701. Average Waiting Time.cpp
+++ 1701-average-waiting-time/1701. Average Waiting Time.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     double averageWaitingTime(vector<vector<int>>& c) {
+         ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
         double sum = 0;
         int n = c.size();
         int nextprep = c[0][0];
