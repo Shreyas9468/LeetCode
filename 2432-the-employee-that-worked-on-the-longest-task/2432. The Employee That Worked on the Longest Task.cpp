6f8ede6bbb76ef36b828af8c38fabc1f0937f1d4
Shreyas <122095667+Shreyas9468@users.noreply.github.com>
2024-07-20 09:53:02 +0530

    Runtime: 47 ms (80.46%), Memory: 39.6 MB (33.77%) - Gitcode

---
+++ 2432-the-employee-that-worked-on-the-longest-task/2432. The Employee That Worked on the Longest Task.cpp
@@ -0,0 +1,19 @@
+class Solution 
+{
+public:
+    int hardestWorker(int n, vector<vector<int>>& logs) 
+    {
+        int id=INT_MAX, maxi=INT_MIN, last=0;
+        
+        for(int i=0; i<logs.size(); i++)
+        {
+            if((logs[i][1] - last) > maxi || ((logs[i][1] - last)==maxi && id > logs[i][0]))
+            {
+                maxi = (logs[i][1] - last);
+                id = logs[i][0];
+            } 
+            last = logs[i][1];
+        }
+        return id;
+    }
+};
