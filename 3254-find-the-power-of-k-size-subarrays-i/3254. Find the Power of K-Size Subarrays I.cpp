c0cb61c12274f4034ca2a4b85e864bb92b8bc29d
Shreyas <122095667+Shreyas9468@users.noreply.github.com>
2024-08-18 03:17:11 +0530

    Runtime: 15 ms (100.00%), Memory: 32.9 MB (100.00%) - Gitcode

--- 3254-find-the-power-of-k-size-subarrays-i/3254. Find the Power of K-Size Subarrays I.cpp
+++ 3254-find-the-power-of-k-size-subarrays-i/3254. Find the Power of K-Size Subarrays I.cpp
@@ -2,27 +2,26 @@ class Solution {
 public:
     vector<int> resultsArray(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> ans(n - k + 1, -1);
-        
-        for (int i = 0; i <= n - k; i++) {
+       vector<int> ans(n-k+1 , -1);
+        for(int  i = 0;i<=n-k;i++){
             int n1 = nums[i];
             bool valid = true;
-            int maxele = nums[i];
-            
-            for (int m = i + 1; m < i + k; m++) {  // Changed 'i < min(k, n)' to 'm < i + k'
-                maxele = max(maxele, nums[m]);
-                if (n1 + 1 != nums[m]) {
+            int maxele = n1;
+            for(int m = i+1;m<min(i+k,n);m++){
+                maxele = max(maxele , nums[m]);
+                if(n1+1 != nums[m]){
                     valid = false;
                     break;
-                } else {
+                }
+                else{
                     n1 = nums[m];
                 }
             }
-            if (valid) {
+            if(valid){
                 ans[i] = maxele;
             }
         }
-        
         return ans;
+
     }
 };
