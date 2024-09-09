4be7e6798fb691e2a6e276189dd48e7fd597c02f
Shreyas <122095667+Shreyas9468@users.noreply.github.com>
2024-09-09 10:05:46 +0530

    Runtime: 2 ms (100.00%), Memory: 42.2 MB (100.00%) - Gitcode

---
+++ 3280-convert-date-to-binary/3280. Convert Date to Binary.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String convertDateToBinary(String date) {
+        String y = date.substring(0,4);
+        String m = date.substring(5,7);
+        String d = date.substring(8,10);
+        StringBuilder sb = new StringBuilder();
+        sb.append(Integer.toBinaryString(Integer.parseInt(y)));
+        sb.append("-");
+        sb.append(Integer.toBinaryString(Integer.parseInt(m)));
+        sb.append("-");
+        sb.append(Integer.toBinaryString(Integer.parseInt(d)));
+        return sb.toString();
+    }
+}
